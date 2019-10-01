c1096acde1af94ea67de97fe3511fa1f08907568
giuseppe <giuseppe.bifano.99@gmail.com>
2019-10-01 11:32:23 +0200

    added registrazioneComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrazioneComponent } from './componenti/registrazione/registrazione.component';
+import { MatFormFieldModule, MatInputModule} from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrazioneComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componenti/registrazione/registrazione.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators, AbstractControl, FormArray } from '@angular/forms';
+
+export function emailDomainValidator(control: AbstractControl) {
+  let email = control.value;
+  if (!email) {
+    return null;
+  }
+
+  if (email && email.indexOf('@') !== -1) {
+    let [_, domain] = email.split('@');
+
+    if (domain !== 'gmail.com') {
+      return { emailDomain: true };
+
+    } else {
+        return null;
+      }
+
+  }
+  return { malformedEmail: true };
+}
+
+@Component({
+  selector: 'app-registrazione',
+  templateUrl: './registrazione.component.html',
+  styleUrls: ['./registrazione.component.css']
+})
+export class RegistrazioneComponent implements OnInit {
+  validatore: FormGroup;
+  emails: FormArray;
+
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.validatore = this.fb.group({
+      nome: new FormControl('', [Validators.required]),
+      cognome: new FormControl('', [Validators.required]),
+      emails: new FormArray([], [Validators.required, emailDomainValidator])
+    });
+    this.addEmail();
+  }
+
+  createEmail(): FormGroup {
+    return this.fb.group({
+      email: new FormControl('', [Validators.required, emailDomainValidator])
+    });
+  }
+
+  removeEmail(indice: number): void {
+    this.emails.removeAt(indice);
+  }
+
+  addEmail(): void {
+    this.emails = this.validatore.get('emails') as FormArray;
+    this.emails.push(this.createEmail());
+    // this.validEmail.setControl('emails', this.validEmail.get('emails') as FormArray);
+    // console.log(this.validEmail);
+  }
+
+}
